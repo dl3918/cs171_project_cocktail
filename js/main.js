aeee15304c82de92742596df6a65e74de51f4fb0
DanniLai <danninglai@g.harvard.edu>
2023-11-18 22:53:21 -0500

    frame

---
+++ js/main.js
@@ -0,0 +1,12 @@
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling:true,
+    scrollHorizontally: true,
+    fitToSection: true,
+    parallax: true,
+    navigation: true,
+    navigationPosition: 'right',
+
+    licenseKey: "gplv3-license",
+    anchors: ['page1', 'page2','page3','page4','page5','page6','page7']
+});
