37cccc4b4723e6f440570c0d64264554f23bbd93
April Chang <aprilchang@g.harvard.edu>
2023-11-19 22:56:45 -0500

    Add network page

--- js/main.js
+++ js/main.js
@@ -10,3 +10,24 @@ new fullpage('#fullpage', {
     licenseKey: "gplv3-license",
     anchors: ['page1', 'page2','page3','page4','page5','page6','page7']
 });
+
+let promises = [
+    d3.csv('data/network/network_ingredients.csv'),
+    d3.csv('data/network/ingredient_relationships.csv')
+]
+let dataArray = []
+Promise.all(promises)
+    .then(function (data) {
+        dataArray = data
+        initMainPage(dataArray)
+    })
+    .catch(function (err) {
+        console.log(err)
+    });
+
+let myNetworkVis;
+
+
+function initMainPage(dataArray) {
+    myNetworkVis = new NetworkVis('networkDiv', dataArray[0], dataArray[1]);
+}
