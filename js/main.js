96436d7b4a330a46c909d34d5af7dc4bd1a14681
April Chang <aprilchang@g.harvard.edu>
2023-11-27 05:50:28 -0500

    Adjusted network vis

--- js/main.js
+++ js/main.js
@@ -19,6 +19,8 @@ let promises = [
     d3.json('data/all_drink_clean.json'),
     d3.json('data/treemap_data.json'),
     d3.csv('data/data_cocktails_cleaned.csv')
+    d3.csv('data/data_cocktails_cleaned.csv'),
+    d3.csv('data/mixology/essential_ingredients.csv'),
 ]
 let dataArray = []
 Promise.all(promises)
@@ -43,7 +45,7 @@ let myIconVis;
 let myallBubbleChart_enoch;
 let myTreeMap;
 let myTasteVis;
-
+let myMixologyVis;
 
 function initMainPage(dataArray) {
     const groupedByAlcType = groupByAlcType(dataArray[2]);
@@ -56,7 +58,8 @@ function initMainPage(dataArray) {
     myTreeMap = new TreeMap('treemapDiv', dataArray[4]);
     myTasteVis = new tasteBubbleChart('taste', dataArray[5]);
     myNetworkVis = new NetworkVis('networkDiv', dataArray[0], dataArray[1]);
-    //myNetworkVis = new NetworkVis('networkDiv', dataArray[0], dataArray[1]);
+    myMixologyVis = new MixologyVis('mixologyDiv', dataArray[6], dataArray[7]);
+
     document.getElementById('resetViewButton').addEventListener('click', () => {
         myBubbleChart.resetView();
     });
