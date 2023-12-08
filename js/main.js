13041764f200713d6abb1173172c64e44095d690
Enoch Liu <TBENOCH@GMAIL.COM>
2023-12-08 17:23:49 -0500

    not display myTasteVis

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ let myAllBubbleChart;
 let myIconVis;
 let myallBubbleChart_enoch;
 let myTreeMap;
-let myTasteVis;
+// let myTasteVis;
 let myMixologyVis;
 
 function initMainPage(dataArray) {
@@ -49,7 +49,7 @@ function initMainPage(dataArray) {
     //myAllBubbleChart = new BubbleChart('allDrink', dataArray[2],true);
     myallBubbleChart_enoch = new allBubbleChart('all_drinks_enoch', dataArray[3]);
     myTreeMap = new TreeMap('treemapDiv', dataArray[4]);
-    myTasteVis = new tasteBubbleChart('taste', dataArray[5]);
+    // myTasteVis = new tasteBubbleChart('taste', dataArray[5]);
     myNetworkVis = new NetworkVis('networkDiv', dataArray[0], dataArray[1]);
 
     // document.getElementById('resetViewButton').addEventListener('click', () => {
