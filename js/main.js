f378dbe48ad06d6a827272fd6a20dbe24dd61710
Enoch Liu <TBENOCH@GMAIL.COM>
2023-12-11 04:01:35 -0500

    add mixlogy transpage

--- js/main.js
+++ js/main.js
@@ -14,17 +14,8 @@ new fullpage('#fullpage', {
     afterLoad: function(origin, destination, direction){
         // 检查是否是特定的section
         if(destination.item.id === 'section1-transpage'){
-            // 开始第一行的动画
-            const line1 = document.getElementById('line1');
-            line1.style.visibility = 'visible';
-            line1.style.animation = 'typing 1.5s steps(150, end) forwards';
-
-            // 第一行动画结束后开始第二行的动画
-            line1.addEventListener('animationend', () => {
-                const line2 = document.getElementById('line2');
-                line2.style.visibility = 'visible';
-                line2.style.animation = 'typing 2s steps(60, end) forwards';
-            });
+            const lines = document.querySelectorAll('#section1-transpage .line');
+            startTypingEffect(lines);
         }
 
         if(destination.item.id === 'section-popular-transpage'){
@@ -44,9 +35,23 @@ new fullpage('#fullpage', {
                 }, index * 600); // 每行间隔1秒
             });
         }
+
+        // 针对section-mixology-transpage的动画（逐字打印效果）
+        if(destination.item.id === 'section-mixology-transpage'){
+            const lines = document.querySelectorAll('#section-mixology-transpage .line');
+            startTypingEffect(lines);
+        }
     }
 });
 
+function startTypingEffect(lines) {
+    lines.forEach((line, index) => {
+        setTimeout(() => {
+            line.style.visibility = 'visible';
+            line.style.animation = `typing 2s steps(${line.textContent.length}, end) forwards, blink-caret 0.75s step-end infinite`;
+        }, index * 2000); // Adjust the delay as needed
+    });
+}
 
 let promises = [
     d3.json('data/network/nodes.json'), //0
